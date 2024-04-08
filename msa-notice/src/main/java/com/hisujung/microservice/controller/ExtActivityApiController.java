c4c4efe01084271c0571722be599ead682864c59
Leeyoujin00 <cymn6032@gmail.com>
2024-04-08 16:27:51 +0900

    refactor: 컨트롤러 엔드포인트 url 수정

--- msa-notice/src/main/java/com/hisujung/microservice/controller/ExtActivityApiController.java
+++ msa-notice/src/main/java/com/hisujung/microservice/controller/ExtActivityApiController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Slf4j
-@RequestMapping("/externalact")
+@RequestMapping("notice/externalact")
 @RequiredArgsConstructor
 @RestController
 public class ExtActivityApiController {
