0293f79982a4b74ac803d2332c9d321f801baa92
Leeyoujin00 <cymn6032@gmail.com>
2024-04-12 14:12:18 +0900

    feat: 회원의 대외활동/교내활동 참여 여부 create, read, delete 기능 구현

--- msa-notice/src/main/java/com/hisujung/microservice/repository/UnivActivityRepository.java
+++ msa-notice/src/main/java/com/hisujung/microservice/repository/UnivActivityRepository.java
@@ -10,12 +10,12 @@ import java.util.List;
 public interface UnivActivityRepository extends JpaRepository<UnivActivity, Long> {
 
     //교내 공지사항 학과별로 검색
-    List<UnivActivity> findByPostDepartmentContaining(String department);
+    List<UnivActivity> findByDepartmentContaining(String department);
 
     //교내 전체 공지사항 제목 키워드로 검색
     List<UnivActivity> findByTitleContaining(String keyword);
 
     //교재 공지사항 학과와 제목 키워드로 검색
-    @Query("SELECT u FROM UnivActivity u WHERE u.postDepartment LIKE %:dep% AND u.title LIKE %:keyword% ORDER BY u.id DESC")
+    @Query("SELECT u FROM UnivActivity u WHERE u.department LIKE %:dep% AND u.title LIKE %:keyword% ORDER BY u.id DESC")
     List<UnivActivity> findByDepartmentAndTitle(@Param("dep") String department, @Param("keyword") String keyword);
 }
