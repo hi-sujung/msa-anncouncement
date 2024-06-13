6a46d99e7c3b949e7e4e655166fa45b71c5de6f7
Jonjae <80445063+EunjaeJo@users.noreply.github.com>
2024-06-13 20:49:13 +0900

    fix: CORS 정책 설정

--- msa-notice/src/main/java/com/hisujung/microservice/config/SecurityConfig.java
+++ msa-notice/src/main/java/com/hisujung/microservice/config/SecurityConfig.java
@@ -1,46 +1,21 @@
-package com.hisujung.microservice.config;
-
-import lombok.RequiredArgsConstructor;
+package com.hisujung.microservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
-@RequiredArgsConstructor
-@EnableWebSecurity
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
-    private static final String[] AUTH_WHITELIST = {
-            "/portfolio/**"
-    };
-
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        // CSRF 비활성화
-        http.csrf(csrf -> csrf.disable());
-
-        // CORS 설정
-        http.cors(cors -> cors.configurationSource(corsConfigurationSource()));
-
-        // 세션 관리 상태 없음으로 구성, Spring Security가 세션 생성 or 사용 X
-        // http.sessionManagement(sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-
-        // FormLogin, BasicHttp 비활성화
-        http.formLogin(form -> form.disable());
-        http.httpBasic(httpBasic -> httpBasic.disable());
-
-        // 권한 규칙 작성
-        http.authorizeHttpRequests(authorize -> authorize
-                .requestMatchers(AUTH_WHITELIST).permitAll()
-                .anyRequest().permitAll()
-        );
-
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()));
         return http.build();
     }
 
